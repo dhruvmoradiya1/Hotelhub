dd70197dd6cbae142603d287eb3bff2a795f23a9
dhruvmoradiya69000 <dhruvmoradiya69000@gmail.com>
2024-11-11 10:58:58 +0530

    notheng

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || process.env.FRONTEND_URL_NGINX,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
